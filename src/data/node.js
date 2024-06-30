2ae45faf190a909e47be2c8eb7bfcf36e7701906
Andrew <33224676+andbor2016@users.noreply.github.com>
2024-06-30 15:08:17 +0300

    Create node.js
    Add nodejs info

---
+++ src/data/node.js
@@ -0,0 +1,26 @@
+export const nodejsCollection = {
+	name: "Node.js",
+  areas: [{
+    name: "Basic",
+    skills: [{
+      name: "Tutorials",
+      description: {
+        test: "Learn basic info by them next tutorials",
+        links: [
+          ["Node.js Tutorial (Codevolution)", "https://www.youtube.com/watch?v=LAUi8pPlcUM&list=PLC3y8-rFHvwh8shCMHFA5kWxD9PaPwxaY"],
+        ]
+      }
+    }]
+  },{
+		name: "Testing",
+		skills: [{
+      name: "Unit tests",
+			description: {
+			  text: "Learn how to write unit tests.",
+				links: [
+          ["Nest.js Unit Testing | Best Practices + MongoDB", "https://www.youtube.com/watch?v=1Vc6Xw8FMpg"],						
+				]
+			},
+    }]
+  ]
+}
